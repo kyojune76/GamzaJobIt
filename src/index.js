d30c496d894c7219cdb34854c749b96913b8728c
kyojune76 <kyojune1197@naver.com>
2025-03-16 23:40:36 +0900

    로직1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./App";
+
 import Signup from "./SignUp";
-import App from "./App";
+import Home from "./Home";
 import LocalSignuPage from "./LocalSignuPage";
 import SignUpLocal from "./SignUpLocal";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,12 +16,9 @@ root.render(
         <Route path="/signup" element={<Signup />} />
         <Route path="/lsp" element={<LocalSignuPage />} />
         <Route path="/signuplocal" element={<SignUpLocal />} />
-        <Route path="/localsignupage" element={<LocalSignuPage />} />
       </Routes>
     </Router>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
